ec3dc332506aaebd0fccb4a3e7dcf2d952aff2bd
Mortharion-tech <bjoernmueller1992@yahoo.de>
2024-11-14 22:46:38 +0100

    showData function

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ const bear3 = document.getElementById("bear3");
 async function getData(params) {
     let img1, img2, img3;
     try {
-        img1 = await fetch('https://placebear.com/200/300', { mode: 'no-cors' });
+        img1 = await fetch('https://placebear.com/200/300');
         img2 = await fetch('https://placebear.com/300/450', { mode: 'no-cors' });
         img3 = await fetch('https://placebear.com/500/750', { mode: 'no-cors' });
         console.log(img1);
@@ -16,8 +16,15 @@ async function getData(params) {
 }
 
 async function showData(params) {
-    const { img1, img2, img3 } = await getData();
-    bear1.src=img1.url;
+    try {
+        const { img1, img2, img3 } = await getData();
+        bear1.src=img1.url;
+        bear2.src=img2.url;
+        bear3.src=img3.url;
+    } catch(err) {
+        console.log(err);
+    }
+    
 }
 
 showData();
