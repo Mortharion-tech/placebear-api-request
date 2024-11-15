5a4403ba9f832b1e5e7d7d91389758ea500238d5
Mortharion-tech <bjoernmueller1992@yahoo.de>
2024-11-15 22:13:29 +0100

    added getImage function

--- javascript.js
+++ javascript.js
@@ -4,13 +4,27 @@ const bear3 = document.getElementById("bear3");
 
 let proxyUrl = 'https://cors-anywhere-proxy.com/';
 
+async function getImage(url) {
+    try {
+        let response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.url;
+    } catch(err) {
+        console.error(err);
+        throw err;
+    }
+}
+
 async function getData(params) {
     let img1, img2, img3;
     try {
-        img1 = await fetch(proxyUrl + 'https://placebear.com/200/300');
-        img2 = await fetch('https://placebear.com/300/450', { mode: 'no-cors' });
-        img3 = await fetch('https://placebear.com/500/750', { mode: 'no-cors' });
+        img1 = await getImage('https://placebear.com/200/300');
+        img2 = await getImage('https://placebear.com/300/450');
+        img3 = await getImage('https://placebear.com/500/750');
         console.log(img1);
+        console.log(img2);
         return { img1, img2, img3 };
     } catch(err) {
         console.log(err);
